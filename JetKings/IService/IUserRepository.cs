8419e9cbc6daf4153548894d5f87098262d70cd8
Shaurya Dhingra <shaurya.dhingra@cginfinity.com>
2026-07-11 14:50:37 +0530

    Remove default User apis

--- JetKings/IService/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using JetKings.Models;
-
-namespace JetKings.IService;
-
-public interface IUserRepository : IGenericRepository<User>
-{
-    Task<User?> GetByEmailAsync(string email, CancellationToken ct = default);
-    Task<bool> EmailExistsAsync(string email, CancellationToken ct = default);
-}
