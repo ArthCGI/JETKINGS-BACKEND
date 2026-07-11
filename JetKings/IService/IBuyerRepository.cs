b9d0dceac60639dead032fee2bd1e9ce1ed92718
Arth  Sharma <arth.sharma@cginfinity.com>
2026-07-11 13:25:45 +0530

    Added Buyer CRUD APIs with repository, service, DTOs and controller and scaffolded the entire database into entity folder

---
+++ JetKings/IService/IBuyerRepository.cs
@@ -0,0 +1,14 @@
+﻿using JetKings.Entity;
+namespace JetKings.IService
+{
+    public interface IBuyerRepository
+    {
+
+        Task<IEnumerable<Buyer>> GetAllAsync();
+        Task<Buyer?> GetByIdAsync(int id);
+        Task<Buyer> CreateAsync(Buyer buyer);
+        Task UpdateAsync(Buyer buyer);
+        Task DeleteAsync(Buyer buyer);
+
+    }
+}
---
+++ JetKings/IService/IBuyerService.cs
@@ -0,0 +1,26 @@
+﻿using JetKings.Models.DTOs;
+
+namespace JetKings.IService;
+
+public interface IBuyerService
+{
+    Task<ApiResponseDto<BuyerResponseDto>> GetByIdAsync(int id, CancellationToken ct = default);
+
+    Task<ApiResponseDto<PagedResponseDto<BuyerResponseDto>>> GetAllAsync(
+        int page,
+        int pageSize,
+        CancellationToken ct = default);
+
+    Task<ApiResponseDto<BuyerResponseDto>> CreateAsync(
+        CreateBuyerRequestDto dto,
+        CancellationToken ct = default);
+
+    Task<ApiResponseDto<BuyerResponseDto>> UpdateAsync(
+        int id,
+        UpdateBuyerRequestDto dto,
+        CancellationToken ct = default);
+
+    Task<ApiResponseDto<bool>> DeleteAsync(
+        int id,
+        CancellationToken ct = default);
+}
